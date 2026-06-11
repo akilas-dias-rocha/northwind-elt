febe26e4094b8b90054a8733257f54d1610be758
Akilas <akilasrocha.eng@gmail.com>
2026-06-11 19:55:54 -0300

    feat: add 'date_name' and 'date_short_name' columns

--- dbt/models/marts/dim__date.sql
+++ dbt/models/marts/dim__date.sql
@@ -16,7 +16,7 @@ with
             , extract(year from date_day) as year
             , extract(quarter from date_day) as quarter
             , dayofyear(date_day) as day_of_year
-            , date_format(date_day, 'MMM') as month_name
+            , date_format(date_day, 'MMM') as month_short_name
             , date_format(date_day, 'dd-MM') as day_month
         from date_spine_raw
     )
@@ -33,6 +33,20 @@ with
             when 6 then 'Friday'
             when 7 then 'Saturday'
         end as week_day_name
+        , case month
+            when 1 then 'January'
+            when 2 then 'February'
+            when 3 then 'March'
+            when 4 then 'April'
+            when 5 then 'May'
+            when 6 then 'June'
+            when 7 then 'July'
+            when 8 then 'August'
+            when 9 then 'September'
+            when 10 then 'October'
+            when 11 then 'November'
+            when 12 then 'December'
+        end as month_name
         , concat('Q', cast(quarter as string)) as quarter_name
     from days_info
 )
